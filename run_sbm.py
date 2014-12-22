8b6d0b3d229f3848e97fdb1fbb9f595edf20ecce
Florian Geigl <florian.geigl@student.tugraz.at>
2014-12-22 12:12:40 +0100

    sbm etc.

--- run_sbm.py
+++ run_sbm.py
@@ -34,23 +34,25 @@ def get_ranking(vp_map):
 
 
 def main():
-    nodes = 200
-    groups = 2
-    step = 0.01
+    nodes = 500
+    groups = 5
+    step = 0.1
     max_con = 1
+    pairs_reduce = 0.1
+    max_runs = 1000
     results_df = pd.DataFrame()
     results_df.index = results_df.index.astype('float')
-    correlation_perc = 0.25
-    for self_con in np.arange(step, max_con + (step * 0.9), step):
+    correlation_perc = -1
+    for self_con in np.arange(0, max_con + (step * 0.9), step):
         self_con = max_con - self_con
         print 'gen graph with ', nodes, 'nodes.(self:', self_con, ',other:', max_con - self_con, ')'
         network = graph_gen(self_con, max_con - self_con, nodes, groups)
-        cf = cost_function.CostFunction(network, pairs_reduce=0.05, ranking_weights=[np.power(i, 2) for i in reversed(range(network.num_vertices()))], verbose=0)
+        cf = cost_function.CostFunction(network, pairs_reduce=pairs_reduce, ranking_weights=[np.power(i, 2) for i in reversed(range(network.num_vertices()))], verbose=0)
         mover = moves.MoveTravelSM(verbose=0)
         all_nodes = range(network.num_vertices())
         random.shuffle(all_nodes)
         print 'optimizing...'
-        opt = optimizer.SimulatedAnnealing(cf, mover, all_nodes, known=0.1, max_runs=1000, reduce_step_after_fails=0, reduce_step_after_accepts=100, verbose=0)
+        opt = optimizer.SimulatedAnnealing(cf, mover, all_nodes, known=0.1, max_runs=max_runs, reduce_step_after_fails=0, reduce_step_after_accepts=100, verbose=0)
         ranking, cost = opt.optimize()
         print 'runs:', opt.runs
         # print 'best ranking', ranking
@@ -76,10 +78,10 @@ def main():
         vp_map, _ = betweenness(network)
         df['betweeness'] = get_ranking(vp_map)
 
-        print df.head()
-        print 'calc correlation between top:', correlation_perc
-        df = df.loc[0:int(round(len(df) * correlation_perc))]
-        correlations = df.corr()
+        if correlation_perc > 0:
+            print 'calc correlation between top:', correlation_perc
+            df = df.loc[0:int(round(len(df) * correlation_perc))]
+        correlations = df.corr(method='spearman')
         create_folder_structure('output/graph_plots/')
         graph_draw(network, output='output/graph_plots/sbm_' + str(self_con) + '.png')
         plt.close('all')
