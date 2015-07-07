6573195007edf68370a3161dbf55371509dbf4cb
Florian Geigl <florian.geigl@student.tugraz.at>
2015-07-07 19:32:29 +0200

    wsdm prep

--- wsdm_main.py
+++ wsdm_main.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from graph_tool.all import *
 from tools.gt_tools import SBMGenerator, load_edge_list, load_property
 import tools.basics as basics
 import multiprocessing
@@ -10,22 +11,67 @@ from data_io import *
 import utils
 
 
+def get_network(name, directed=True):
+    # synthetic params
+    num_links = 1200
+    num_nodes = 300
+    num_blocks = 5
+    price_net_m = 4
+    price_net_g = 1
+    strong_others_con = 0.1
+    weak_others_con = 0.7
+    print 'get network:', name.lsplit('/', 1)[-1]
+    if name == 'toy_example':
+        net = price_network(5, m=2, directed=directed)
+        net.gp['type'] = net.new_graph_property('string', 'synthetic')
+
+    elif name == 'karate':
+        net = load_edge_list('/opt/datasets/karate/karate.edgelist', directed=False)
+        net.gp['type'] = net.new_graph_property('string', 'empiric')
+
+    elif name == 'sbm_strong' or name == 'sbm_weak':
+        if name == 'sbm_weak':
+            other_con = weak_others_con
+        else:
+            other_con = strong_others_con
+        generator = SBMGenerator()
+        net = generator.gen_stock_blockmodel(num_nodes=num_nodes, blocks=num_blocks, num_links=num_links,
+                                             other_con=other_con, directed=directed)
+        net.gp['type'] = net.new_graph_property('string', 'synthetic')
+
+    elif name == 'price_network':
+        net = price_network(num_nodes, m=price_net_m, gamma=price_net_g, directed=directed)
+        net.gp['type'] = net.new_graph_property('string', 'synthetic')
+
+    else:
+        net = load_edge_list(name, directed=directed, vertex_id_dtype=None)
+        net.gp['filename'] = net.new_graph_property('string', name)
+        net.gp['type'] = net.new_graph_property('string', 'empiric')
+
+    l = label_largest_component(net, directed=True)
+    net.set_vertex_filter(l)
+    net.purge_vertices()
+    net.clear_filters()
+    return net
+
+
 def main():
-    generator = SBMGenerator()
+    multip = True  # multiprocessing flag (warning: suppresses exceptions)
     base_outdir = 'output/'
+    empiric_data_dir = '/opt/datasets/'
+
+    datasets = list()
+    datasets.append({'name': 'toy_example'})
+    datasets.append({'name': 'karate'})
+    datasets.append({'name': empiric_data_dir + 'milan_spiele', 'directed': True})
+    datasets.append({'name': empiric_data_dir + 'getdigital', 'directed': True})
+    datasets.append({'name': empiric_data_dir + 'thinkgeek', 'directed': True})
+
+    datasets.append({'name': '/opt/datasets/wikiforschools/graph', 'directed': True})
+    #datasets.append({'name': '/opt/datasets/facebook/facebook', 'directed': False})
+
     basics.create_folder_structure(base_outdir)
 
-    first_two_only = True  # quick test flag (disables multiprocessing to get possibles exceptions)
-    toy_example = False
-    multip = True  # multiprocessing flag (warning: suppresses exceptions)
-    synthetic = False
-    empiric_crawled = True
-    empiric_downloaded = False
-    karate_club = True
-    if first_two_only:
-        multip = False
-        synthetic = True
-        karate_club = True
     if multip:
         worker_pool = multiprocessing.Pool(processes=14)
     else:
@@ -42,173 +88,20 @@ def main():
     if os.path.isfile(network_prop_file):
         os.remove(network_prop_file)
 
-    num_links = 1200
-    num_nodes = 300
-    num_blocks = 5
-
-    if toy_example:
-        # strong sbm ============================================
-        print 'toy_example'.center(80, '=')
-        name = 'toy_example'
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = price_network(5, m=2, directed=False)
-        net.gp['type'] = net.new_graph_property('string')
+    for ds in datasets:
+        network_name = ds['name']
+        print network_name.center(80, '=')
+        out_dir = base_outdir + network_name + '/'
+        basics.create_folder_structure(out_dir)
+        ds.pop("name", None)
+        net = get_network(network_name, **ds)
         if multip:
             worker_pool.apply_async(self_sim_entropy, args=(net,),
-                                    kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
+                                    kwds={'name': network_name, 'out_dir': out_dir, 'biases': biases, 'error_q': error_q},
                                     callback=async_callback)
         else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-
-    if karate_club:
-        # karate ninja bam bam ============================================
-        print 'karate'.center(80, '=')
-        name = 'karate'
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = load_edge_list('/opt/datasets/karate/karate.edgelist', directed=False)
-        net.gp['type'] = net.new_graph_property('string', 'empiric')
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,),
-                                    kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-
-    if synthetic:
-
-        # strong sbm ============================================
-        print 'sbm strong'.center(80, '=')
-        name = 'sbm_strong_n' + str(num_nodes) + '_m' + str(num_links)
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = generator.gen_stock_blockmodel(num_nodes=num_nodes, blocks=num_blocks, num_links=num_links, other_con=0.1)
-        net.gp['type'] = net.new_graph_property('string', 'synthetic')
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,),
-                                    kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-
-        if first_two_only:
-            exit()
-
-        # weak sbm ============================================
-        print 'sbm weak'.center(80, '=')
-        name = 'sbm_weak_n' + str(num_nodes) + '_m' + str(num_links)
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = generator.gen_stock_blockmodel(num_nodes=num_nodes, blocks=num_blocks, num_links=num_links, other_con=0.7)
-        net.gp['type'] = net.new_graph_property('string', 'synthetic')
-
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,),
-                                    kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-
-        # price network ============================================
-        print 'price network'.center(80, '=')
-        name = 'price_net_n' + str(num_nodes) + '_m' + str(net.num_edges())
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = price_network(num_nodes, m=4, gamma=1, directed=False)
-        net.gp['type'] = net.new_graph_property('string', 'synthetic')
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,),
-                                    kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-
-    if empiric_crawled:
-        empiric_data_dir = '/opt/datasets/'
-        empiric_data_sets = list()
-        empiric_data_sets.append('milan_spiele')
-        empiric_data_sets.append('getdigital')
-        empiric_data_sets.append('thinkgeek')
-
-        for name in empiric_data_sets:
-            print name.center(80, '=')
-            fname = empiric_data_dir + name + '/' + name + '.gt'
-            net = load_graph(fname)
-            l = label_largest_component(net, directed=True)
-            net.set_vertex_filter(l)
-            net.purge_vertices()
-            net.gp['filename'] = net.new_graph_property('string', fname)
-            net.gp['type'] = net.new_graph_property('string', 'empiric')
-            outdir = base_outdir + name + '/'
-            basics.create_folder_structure(outdir)
-            if multip:
-                worker_pool.apply_async(self_sim_entropy, args=(net,),
-                                        kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                        callback=async_callback)
-            else:
-                results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-            write_network_properties(net, name, network_prop_file)
-            # generator.analyse_graph(net, outdir + name, draw_net=False)
-    if empiric_downloaded:
-        # wiki4schools ============================================
-        print 'wiki4schools'.center(80, '=')
-        name = 'wiki4schools'
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = load_edge_list('/opt/datasets/wikiforschools/graph', directed=True)
-        # net.vp['com'] = load_property(net, '/opt/datasets/wikiforschools/artid_catid', type='int')
-        net.gp['type'] = net.new_graph_property('string', 'empiric')
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,), kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-
-        # facebook ============================================
-        print 'facebook'.center(80, '=')
-        name = 'facebook'
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = load_edge_list('/opt/datasets/facebook/facebook', directed=False)
-        # net.vp['com'] = load_property(net, '/opt/datasets/facebook/facebook_com', type='int', line_groups=True)
-        net.gp['type'] = net.new_graph_property('string', 'empiric')
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,), kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-        '''
-        # enron ============================================
-        print 'enron'.center(80, '=')
-        name = 'enron'
-        outdir = base_outdir + name + '/'
-        basics.create_folder_structure(outdir)
-        net = load_edge_list('/opt/datasets/enron/enron')
-        net.gp['type'] = net.new_graph_property('string', 'empiric')
-        # net.vp['com'] = load_property(net, '/opt/datasets/youtube/youtube_com', type='int', line_groups=True)
-        print 'vertices:', net.num_vertices()
-        if multip:
-            worker_pool.apply_async(self_sim_entropy, args=(net,), kwds={'name': name, 'out_dir': outdir, 'biases': biases, 'error_q': error_q},
-                                    callback=async_callback)
-        else:
-            results.append(self_sim_entropy(net, name=name, out_dir=outdir, biases=biases, error_q=error_q))
-        write_network_properties(net, name, network_prop_file)
-        # generator.analyse_graph(net, outdir + name, draw_net=False)
-        '''
+            results.append(self_sim_entropy(net, name=network_name, out_dir=out_dir, biases=biases, error_q=error_q))
+        write_network_properties(net, network_name, network_prop_file)
 
     if multip:
         worker_pool.close()
@@ -228,13 +121,12 @@ def main():
     gini_dfs.to_csv(base_outdir + 'gini_coefs.csv')
     # col_names.append(list(filter(lambda x: 'karate' in x, df.columns))[0])
     col_names = list()
-    col_names.append(list(filter(lambda x: 'price_net_n' in x, df.columns))[0])
-    col_names.append(list(filter(lambda x: 'sbm_weak_n' in x, df.columns))[0])
-    col_names.append(list(filter(lambda x: 'sbm_strong_n' in x, df.columns))[0])
-    gini_to_table(gini_dfs, base_outdir + 'gini_table.txt', digits=2, columns=col_names)
+
+    #col_names.append(list(filter(lambda x: 'price_net_n' in x, df.columns))[0])
+    #col_names.append(list(filter(lambda x: 'sbm_weak_n' in x, df.columns))[0])
+    #col_names.append(list(filter(lambda x: 'sbm_strong_n' in x, df.columns))[0])
+    #gini_to_table(gini_dfs, base_outdir + 'gini_table.txt', digits=2, columns=col_names)
     # import filter_output
-
-
 
 if __name__ == '__main__':
     start = datetime.datetime.now()
