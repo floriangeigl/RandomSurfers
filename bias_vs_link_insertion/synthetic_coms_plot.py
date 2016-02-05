bc9bd3a7cbe74a650c464d58375fe18da19275f8
Florian <florian.geigl@tugraz.at>
2016-02-05 14:53:38 +0100

    plot updates

--- bias_vs_link_insertion/synthetic_coms_plot.py
+++ bias_vs_link_insertion/synthetic_coms_plot.py
@@ -184,12 +184,12 @@ def plot_dataframe(df_fn, net, bias_strength, filename):
             ac = ax.scatter(x, y, c=c, lw=0, alpha=0.7, cmap='coolwarm')
             ax.set_xticks(sorted(set(x)), minor=True)
             cbar = plt.colorbar(ac)
-            plt.xlabel('sample size')
+            plt.xlabel(r'$\phi$')
             plt.xlim([0, df_plot['sample-size'].max() + 0.01])
             y_range_one_perc = (df_plot[col_name].max() - df_plot[col_name].min()) * 0.01
             plt.ylim([df_plot[col_name].min() - y_range_one_perc, df_plot[col_name].max() + y_range_one_perc])
             plt.ylabel(col_name.replace('_', ' '))
-            cbar.set_label(r'sample size standardized $\sum \pi$' if normed_stat_dist else r'$\pi_G$')
+            cbar.set_label(r'\phi standardized $\sum \pi$' if normed_stat_dist else r'$\pi_G$')
             # cbar.set_label('$\\frac{\\sum \\pi_b}{\\sum \\pi_{ub}}$')
 
             plt.title(ds_name + '\nBias Strength: ' + str(int(bias_strength)))
@@ -229,46 +229,27 @@ def plot_lines_plot(df, x_col_name, y_col_name, out_fn_base, out_fn_ext, one_sub
                     fig_size=(16, 10), label_dict=None, ds_name='', plt_std=True, plot_histo=True):
     if x_col_name not in {'ratio_com_out_deg_in_deg', 'com_in_deg', 'com_out_deg'}:
         return
+    if 'ratio' not in x_col_name:
+        return
     default_font_size = matplotlib.rcParams['font.size']
     matplotlib.rcParams.update({'font.size': plt_font_size})
     if label_dict is None:
         label_dict = dict()
-    if one_subplot:
-        if plot_histo:
-            fig = plt.figure()
-            gs = gridspec.GridSpec(2, 1, height_ratios=[1, 3])
-            ax1 = plt.subplot(gs[0])
-            ax2 = plt.subplot(gs[1])
-        else:
-            fig, ax2 = plt.subplots()
-            ax1 = None
-    else:
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=fig_size)
-    if not one_subplot:
-        ax1.plot([np.nan], [np.nan], label='sample-size', c='white')
-    ax2.plot([np.nan], [np.nan], label='sample-size', c='white')
-    # lw_func = lambda x: 1. + ((x - .01) / (.2 - .01)) * 3
+    fig = plt.figure()
+    gs = gridspec.GridSpec(3, 1, height_ratios=[1, 2, 2])
+    ax1 = fig.add_subplot(gs[0])
+    ax2 = fig.add_subplot(gs[1], sharex=ax1)
+    ax3 = fig.add_subplot(gs[2], sharex=ax2)
+    ax3.plot([np.nan], [np.nan], label=r'\phi', c='white', alpha=0.)
+
     lw_func = lambda x: 2
     legend_plot = True
-    all_sample_sizes = set(df['sample-size'])
-    if False:
-        if 'ratio' in x_col_name:
-            min_x_val, max_x_val = 0.4, 2.
-            df = df[(df[x_col_name] <= max_x_val) & (df[x_col_name] >= min_x_val)]
-        else:
-            filtered_sample_size = set(sorted(all_sample_sizes)[::2])
-            legend_plot = False
-            df = df[df['sample-size'].apply(lambda x: x in filtered_sample_size)]
-            min_x_val, max_x_val = df[x_col_name].min(), df[x_col_name].max()
-            # print 'sample-sizes:', sorted(set(df['sample-size']))
-    else:
-        min_x_val, max_x_val = df[x_col_name].min(), df[x_col_name].max()
+
+    min_x_val, max_x_val = df[x_col_name].min(), df[x_col_name].max()
     min_y_val, max_y_val = df[y_col_name].min(), df[y_col_name].max()
     num_bins = 6
     x_val_range = max_x_val - min_x_val
     y_val_range = max_y_val - min_y_val
-
-    x_annot_offset = x_val_range / 15
 
     if 'ratio' in x_col_name:
         plt_x_range = [min_x_val, max_x_val]
@@ -279,7 +260,7 @@ def plot_lines_plot(df, x_col_name, y_col_name, out_fn_base, out_fn_ext, one_sub
     y_offset = y_val_range / 100 * 2
     plt_y_range = [min_y_val - y_offset, max_y_val + y_offset]
 
-    plt_x_center = plt_x_range[0] + ((plt_x_range[1] - plt_x_range[0]) / 2)
+    # plt_x_center = plt_x_range[0] + ((plt_x_range[1] - plt_x_range[0]) / 2)
     df['sample-size'] = np.round(df['sample-size'], decimals=3)
     sample_sizes = {0.01, 0.1, 0.2}
     df = df[map(lambda x: x in sample_sizes, df['sample-size'])]
@@ -287,10 +268,6 @@ def plot_lines_plot(df, x_col_name, y_col_name, out_fn_base, out_fn_ext, one_sub
     markers = "o^s"
     print(x_col_name, y_col_name, sorted(set(df['sample-size'])))
     for style_idx, (key, grp) in enumerate(df[['sample-size', x_col_name, y_col_name]].groupby('sample-size')):
-        use_arrows = False
-        rnd_label_pos = True
-        annotate = False
-
         key = np.round(key, decimals=3)
         if key not in sample_sizes:
             continue
@@ -303,8 +280,8 @@ def plot_lines_plot(df, x_col_name, y_col_name, out_fn_base, out_fn_ext, one_sub
         start_point = grp_x_min + bins_step_size / 2
         bin_points = np.array([start_point + i * bins_step_size for i in range(num_bins)])
 
-        if not one_subplot:
-            ax1 = grp.plot(x=x_col_name, y=y_col_name, ax=ax1, label='  ' + key_str)
+        #if not one_subplot:
+        #    ax1 = grp.plot(x=x_col_name, y=y_col_name, ax=ax1, label='  ' + key_str)
         c = colors[style_idx % len(colors)]
         m = markers[style_idx % len(markers)]
         grp['bin'] = grp[x_col_name].apply(lambda x: min(int((x - grp_x_min) / bins_step_size), num_bins - 1))
@@ -320,113 +297,86 @@ def plot_lines_plot(df, x_col_name, y_col_name, out_fn_base, out_fn_ext, one_sub
         if key_str == '0.1' and x_col_name == 'ratio_com_out_deg_in_deg' and 'fac' not in y_col_name and 'orf' in ds_name.lower():
             y1 = tmp_grp.iloc[1][y_col_name]
             y0 = tmp_grp.iloc[0][y_col_name]
-            ax2.axhline(y=(y0 + y1) / 2, color='black', lw=2)
-        # if key_str == '0.1' and x_col_name == 'ratio_com_out_deg_in_deg' and 'fac' in y_col_name:
-            # ax2.set_yscale('log')
-            # plt_y_range = lambda x:
-        # print ds_name, x_col_name, key, '\n', tmp_grp
-        label_on_line = False
-        if len(tmp_grp) > 5 and rnd_label_pos:
-            annotate_idx = np.random.randint(1, len(tmp_grp) - 3)
-            label_on_line = True
-        else:
-            annotate_idx = int((len(tmp_grp)-1) / 2)
-        center_row = tmp_grp.iloc[annotate_idx]
-        x_center, y_center = center_row[['bin_center', y_col_name]]
-        annotate_font_size = plt_font_size / 2
-
-        next_x, next_y = tmp_grp.iloc[min(len(tmp_grp) - 1, annotate_idx + 1)][['bin_center', y_col_name]]
-        if len(tmp_grp) % 2 == 0 or label_on_line:
-            x_center += ((next_x - x_center) / 2)
-            y_center += ((next_y - y_center) / 2)
-            last_x, last_y = tmp_grp.iloc[max(0, annotate_idx)][['bin_center', y_col_name]]
-        else:
-            last_x, last_y = tmp_grp.iloc[max(0, annotate_idx - 1)][['bin_center', y_col_name]]
+            ax3.axhline(y=(y0 + y1) / 2, color='black', lw=2)
 
         ax2_plt_kwargs = dict(x='bin_center', y=y_col_name, color=c, lw=lw_func(key), solid_capstyle="round", alpha=.95,
                               label='  ' + key_str, marker=m, markersize=12, markeredgewidth=2,
                               markeredgecolor=(.99, .99, .99, .9))
-        annotate_bbox = dict(boxstyle='round4,pad=0.2', fc='white', ec=c, alpha=0.7)
-        annotate_kwargs = dict(ha='center', va='center', bbox=annotate_bbox, fontsize=annotate_font_size)
-        annotate_arrow = dict(arrowstyle="->, head_width=1.", facecolor='black',
-                              connectionstyle='angle3') #tail_width=.25, shrink_factor=0.05
 
-        if grp_x_max - grp_x_min < x_val_range / 100 * 5 and use_arrows:
-            if len(tmp_grp) == 1:
-                c_bin_center, c_y_val, c_std, c_count = tmp_grp.iloc[0][['bin_center', y_col_name, 'std', 'count']]
-                tmp_grp = pd.DataFrame(columns=['bin_center', y_col_name, 'std', 'count'],
-                                       data=[(c_bin_center - bins_step_size, c_y_val, c_std, c_count),
-                                             (c_bin_center + bins_step_size, c_y_val, c_std, c_count)])
-
+        if key < 0.09:
             ax2 = tmp_grp.plot(ax=ax2, **ax2_plt_kwargs)
-
-            if annotate:
-                ax2.annotate(key_str, xy=(x_center, y_center), xytext=(
-                    (x_center + x_annot_offset) if x_center < plt_x_center else (x_center - x_annot_offset), y_center),
-                         arrowprops=annotate_arrow, **annotate_kwargs)
+            tmp_kwargs = ax2_plt_kwargs.copy()
+            tmp_kwargs.pop('x')
+            tmp_kwargs.pop('y')
+            ax3.plot([np.nan], [np.nan], **tmp_kwargs)
         else:
-            ax2 = tmp_grp.plot(ax=ax2, **ax2_plt_kwargs)
-            x_diff = next_x - last_x
-            y_diff = next_y - last_y
-            x_diff /= (plt_x_range[1] - plt_x_range[0])
-            y_diff /= (plt_y_range[1] - plt_y_range[0])
-            k = y_diff / x_diff
-            rotn = np.degrees(np.arctan(k)) * .8
-            # print 'rotn:', rotn * .9
-            if annotate:
-                ax2.annotate(key_str, xy=(x_center, y_center), xytext=(x_center, y_center), rotation=rotn, **annotate_kwargs)
+            ax3 = tmp_grp.plot(ax=ax3, **ax2_plt_kwargs)
+
         if plt_std:
             tmp_grp['std'].fillna(0., inplace=True)
-            ax2.fill_between(tmp_grp['bin_center'], tmp_grp[y_col_name] - tmp_grp['std'],
-                             tmp_grp[y_col_name] + tmp_grp['std'], color=c, alpha=0.25, label=None, interpolate=True)
-        if plot_histo:
-            ax1 = tmp_grp.plot(x='bin_center', y='count', ax=ax1, lw=lw_func(key), color=c, solid_capstyle="round",
-                               alpha=.95, marker=m, markersize=12)
+            tmp_ax = ax2 if key < 0.09 else ax3
+            tmp_ax.fill_between(tmp_grp['bin_center'], tmp_grp[y_col_name] - tmp_grp['std'],
+                                tmp_grp[y_col_name] + tmp_grp['std'], color=c, alpha=0.25, label=None,
+                                interpolate=True)
+
+        ax1 = tmp_grp.plot(x='bin_center', y='count', ax=ax1, lw=lw_func(key), color=c, solid_capstyle="round",
+                           alpha=.95, marker=m, markersize=12)
     # time.sleep(10)
     # grp_df.plot(x=x_col_name, legend=False)
     x_label = label_dict[x_col_name] if x_col_name in label_dict else x_col_name.replace('_', ' ')
     plt.xlabel(x_label)
     y_label = label_dict[y_col_name] if y_col_name in label_dict else y_col_name.replace('_', ' ')
-    plt.ylabel(y_label)
 
-    if not one_subplot:
-        ax1.legend(loc='best', prop={'size': 12})
-        ax2.legend(loc='best', prop={'size': 12})
-        ax1.set_xlim(plt_x_range)
-        ax1.set_ylim(plt_y_range)
-        ax1.grid(b=True, which='major', axis='y', linewidth=3, alpha=0.2, ls='--')
-        ax1.set_axisbelow(True)
-        plt.title(ds_name)
-    else:
-        ax2.legend_.remove()
-        if plot_histo:
-            ax1.legend_.remove()
-            ax1.set_xlim(plt_x_range)
-            ax1.get_xaxis().set_visible(False)
-            ax1.set_yticks(np.linspace(*ax1.get_ylim(), num=4).astype('int'))
-            ax1.set_xticks([])
-            ax1.set_ylabel('N')
-            ax1.grid(b=True, which='major', axis='y', linewidth=3, alpha=0.2, ls='--')
-            ax1.set_axisbelow(True)
-    ax2.set_xlim(plt_x_range)
-    ax2.set_ylim(plt_y_range)
+    ax2.legend_.remove()
+    ax3.legend_.remove()
+    ax1.legend_.remove()
+    ax1.set_xlim(plt_x_range)
+    ax1.get_xaxis().set_visible(False)
+    ax1.set_yticks(np.linspace(*ax1.get_ylim(), num=3).astype('int'))
+    # ax1.set_xticks([])
+    ax1.set_ylabel('N')
+    ax1.grid(b=True, which='major', axis='y', linewidth=3, alpha=0.2, ls='--')
+    ax1.set_axisbelow(True)
+    ax1.set_xlim(plt_x_range)
+    # ax2.set_ylim(plt_y_range)
     ax2.grid(b=True, which='major', axis='y', linewidth=3, alpha=0.2, ls='--')
+    ax3.grid(b=True, which='major', axis='y', linewidth=3, alpha=0.2, ls='--')
     if 'ratio' in x_col_name:
         plt.xticks()
     else:
         plt.xticks()
         ax2.ticklabel_format(style='sci', axis='x', useOffset=True, useMathText=True, scilimits=(0, 0))
+        ax3.ticklabel_format(style='sci', axis='x', useOffset=True, useMathText=True, scilimits=(0, 0))
     ax2.set_axisbelow(True)
+    ax3.set_axisbelow(True)
 
+    #ax2.set_yticks(np.linspace(*ax2.get_ylim(), num=5))
+    #ax3.set_yticks(np.linspace(*ax3.get_ylim(), num=5))
+
+    #ax1.set_xticks(np.linspace(*ax1.get_xlim(), num=5))
+    from matplotlib.ticker import MaxNLocator, AutoLocator
+
+    ax1.xaxis.set_major_locator(MaxNLocator(nbins=5, prune='both'))
+    ax2.yaxis.set_major_locator(MaxNLocator(nbins=5, prune='both'))
+    ax3.yaxis.set_major_locator(MaxNLocator(nbins=5, prune='both'))
+
+    #ax2.tick_params(axis='y', colors='red')
+    #ax3.tick_params(axis='y', colors='red')
+    #ax2.yaxis.label.set_color('red')
+    #ax3.yaxis.label.set_color('red')
+
+    ax1.tick_params(axis='y', colors='gray')
+    ax1.yaxis.label.set_color('gray')
+
+    ax2.set_ylabel(y_label + '      ', labelpad=30)
     plt.tight_layout(h_pad=0.001)
+    #gs.tight_layout(fig, h_pad=0.001)
+    gs.update(wspace=0.025, hspace=0.2)
     plt_fn = out_fn_base + out_fn_ext
-    if not one_subplot:
-        plt.savefig(plt_fn + '.png', dpi=150)
-    else:
-        plt_tools.save_n_crop(plt_fn + '.pdf')
-        if legend_plot and set(df['sample-size']) == sample_sizes:
-            plt_tools.plot_legend(ax2, out_fn_base.rsplit('/', 2)[0] + '/' + out_fn_ext.strip('_') + '_legend.pdf',
-                                  font_size=12, nrows=1, legend_name_idx=0)
+    plt_tools.save_n_crop(plt_fn + '.pdf')
+    if legend_plot and set(df['sample-size']) == sample_sizes:
+        plt_tools.plot_legend(ax3, out_fn_base.rsplit('/', 2)[0] + '/' + out_fn_ext.strip('_') + '_legend.pdf',
+                              font_size=12, nrows=1, legend_name_idx=0)
     plt.close('all')
     matplotlib.rcParams.update({'font.size': default_font_size})
 
@@ -566,7 +516,7 @@ def plot_inserted_links(df, columns, filename):
                         np.array(grp_mean[i]) + np.array(grp_std[i]), color=c, alpha=0.25, label=None,
                         interpolate=True)
     ax.grid(b=True, which='major', axis='y', linewidth=3, alpha=0.2, ls='--')
-    plt.xlabel('sample size')
+    plt.xlabel(r'$\phi$')
     plt.ylabel(r"stationary prob. ($\pi'_t$)")
     ax.set_xlim([grp_mean.index.min(), grp_mean.index.max()])
     ax.set_axisbelow(True)
@@ -640,7 +590,7 @@ def main():
     net_name = ''
     net = None
     skipped_ds = set()
-    worker_pool = mp.Pool(processes=2)
+    worker_pool = mp.Pool(processes=1)
     # skipped_ds.add('daserste')
     # skipped_ds.add('wiki4schools')
     # skipped_ds.add('tvthek_orf')
